9c363c37f99979a6723865e4acb2feb0e6275554
brian <brian.w.727@gmail.com>
2022-06-10 15:42:41 -0700

    fixed add server button and server item styling.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -4,6 +4,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class ServerElementComponent implements OnInit {
   
